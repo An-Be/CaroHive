aed735a20ebe2df15bf7aefe1735a57e99403488
Andrea <andrea.berrocal95@gmail.com>
2023-05-05 12:21:39 -0700

    sidebar, shop page banner, and display cards

---
+++ client/src/components/Shop/components/ShopBanner/ShopBanner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ShopBanner = ({category}) => {
+  return (
+    <div className="ShopBanner hero max-h-screen" style={{ backgroundImage: `url("/images/stock/photo-1507358522600-9f71e620c44e.jpg")` }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Shop {category}'s</h1>
+      <p className="mb-5">Revamp your style with the latest trends in {category}'s' clothing.</p>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default ShopBanner
